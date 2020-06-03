3a93304f8028a0add088804022b0cdbb3c53925f
Chandrasekhar3333 <chandu.pentyala@gmail.com>
2020-06-03 22:21:10 +0530

    Update ProductDataWriter.java

--- ProductDataWriter.java
+++ ProductDataWriter.java
@@ -1,45 +1,32 @@
 package com.springboot.productdata;
 
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 @Component 
 public class ProductDataWriter {
 	 
-public void writeData(List productsCollection) throws IOException 
+public void writeData(List<Product> productCollection) throws IOException 
 {
-	List<Product> products=new ArrayList<Product>();
-	List<Product> filteredProducts=products.stream().filter(p->p.getProductPrice()>5000).collect(Collectors.toList());
-	 for (Product product : filteredProducts) {
+List<Product> filteredProducts=productCollection.stream().filter(p->p.getProductPrice()>5000).collect(Collectors.toList());
+System.out.println("number of products after filetered "+ ' '+ filteredProducts.size() ); 
+  for (Product product : filteredProducts) {
+		 
 	        System.out.println(product.toString());
-	 }
+	   }
 
-	/*List<Product> filteredProducts=product.stream().filter(p->p.getProductPrice()>5000).collect(Collectors.toList());
-	System.out.println(filteredProducts.toString());
-	//FileWriter writer =new FileWriter("src/main/resources/output.txt");
-	try{
-        FileOutputStream writeData = new FileOutputStream("src/main/resources/output.txt");
-        ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
-
-        writeStream.writeObject(filteredProducts);
-        writeStream.flush();
-        writeStream.close();
-
-    }catch (IOException e) {
+	try {
+		FileWriter writer =new FileWriter("src/main/resources/filteredProducts.txt");
+        writer.write(filteredProducts.toString());
+        writer.close();
+	}
+	catch (IOException e) {
         e.printStackTrace();
 	
-		
-		
-	}*/
+}
 	 
 }
-
-
-
 
 }
